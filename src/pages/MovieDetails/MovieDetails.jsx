d636fceb2263526b233333fb885c0ded3157c640
AlexProkopev <129211387+AlexProkopev@users.noreply.github.com>
2023-11-16 16:49:05 +0100

    debug

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -65,7 +65,7 @@ const MovieDetails = () => {
         /> */}
 
            <img
-                className={css.imgRewievs}
+                className={css.imgPoster}
               src={
                 movie.poster_path
                   ? 'https://image.tmdb.org/t/p/w500' + movie.poster_path
@@ -74,7 +74,12 @@ const MovieDetails = () => {
               alt=""
             />
         <h2 className={css.title}>{movie.original_title}</h2>
+        <h2>Description:</h2>
         <p className={css.description}>{movie.overview}</p>
+        <h2>Rating:</h2>
+        <p className={css.description}>{movie.vote_average}</p>
+        <h2>Genre:</h2>
+        <p className={css.description}>{movie.length !== 0 && movie.genres.map((el,index)=> <span key={index} className={css.spanMap}>{el.name}.</span> )}</p>
       </div>
       <div className={css.wrapperLink}>
         <NavLink to="cast" className={css.linkCastRewievs}>
