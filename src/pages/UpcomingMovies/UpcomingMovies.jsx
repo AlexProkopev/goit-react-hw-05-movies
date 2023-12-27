762a3c8fe0b47f782e6021076726b5d2e1c7b16c
AlexProkopev <129211387+AlexProkopev@users.noreply.github.com>
2023-12-27 09:27:02 +0100

    Update UpcomingMovies.jsx

--- src/pages/UpcomingMovies/UpcomingMovies.jsx
+++ src/pages/UpcomingMovies/UpcomingMovies.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Link, useLocation, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation} from 'react-router-dom';
 import { fetchUpcoming } from 'services/fetchUpcoming';
 import css from './UpcomingMovies.module.css';
 import { defaultImgHome } from 'services/defaultImg';
